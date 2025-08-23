7699d3223178cdef53717d9f51b529426dc6a805
yDh-embedded <ydh.postkasten@yahoo.com>
2025-08-23 11:00:10 +0200

    first commit

---
+++ installer.sh
@@ -0,0 +1,31 @@
+System & Tools:
+
+htop / btop - System-Monitor (schöner als top)
+neofetch - Coole System-Info im Terminal
+tree - Verzeichnisstruktur anzeigen
+wget / curl - Download-Tools
+
+Archive & Dateien:
+
+p7zip - 7-Zip Archive support
+unrar - RAR-Archive öffnen
+thunar oder dolphin - Moderner Dateimanager
+
+Terminal-Verbesserungen:
+
+zsh + oh-my-zsh - Bessere Shell
+tmux - Terminal-Multiplexer
+ranger - Terminal-Dateimanager
+
+Kommunikation:
+
+discord - Gaming/Communities
+telegram-desktop - Messenger
+zoom - Video-Calls
+signal-desktop - Sichere Nachrichten
+
+Backup & Sync:
+
+rsync - Datei-Synchronisation
+rclone - Cloud-Storage-Tool
+timeshift - System-Snapshots
